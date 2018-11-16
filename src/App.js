3956581ca4df3a7d1f55ca4b0296b9a282664fba
Rebeca <rebecaprestes@live.com>
2018-11-15 22:10:43 -0200

    teste5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Divisor extends Component{
+  render(){
+    return(
+      <div>
+        <h4>Rebeca Andrade</h4>
+        <label>Seu nome?</label>
+        <input type="text"></input>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <p>Teste</p>
+      <Divisor/>
     );
   }
 }
