c255b3b865558a9a08101b00d19d0f1bdcc911c8
Rebeca <rebecaprestes@live.com>
2018-11-15 22:06:16 -0200

    teste4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-class MyButton extends Component{
-  render(){
-    return <button>Click Me</button>
-  }
-}
-
-class MyLabel extends Component{
-  render(){
-    return <p>Hello World from Component</p>
-  }
-}
 
 class App extends Component {
   render() {
     return (
-      <p>Teste Rebeca</p>
+      <p>Teste</p>
     );
   }
 }
