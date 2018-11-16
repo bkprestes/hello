7e1fc838a029b3aad05c91b334b37116a76b9ed5
Rebeca <rebecaprestes@live.com>
2018-11-16 10:23:37 -0200

    add form

--- src/ui/ReleaseForm.js
+++ src/ui/ReleaseForm.js
@@ -1,27 +1,33 @@
 import React from 'react';
 
-const ReleaseForm = () => {
+const ReleaseForm = ({submitHandler}) => {
+    let _releaseName, _releaseDate;
+    const handleSubmit = (e) => {
+        submitHandler(e, {
+        name: _releaseName.value, 
+        date: _releaseDate.value
+        });
+        clearForm();
+    }
+
+    const clearForm = () => {
+        _releaseName.value = '';
+        _releaseDate.value = '';
+    }
+
     return(
-        <form class="form-inline">
-            <label class="sr-only" for="inlineFormInputName2">Name</label>
-            <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Jane Doe"/>
+        <form onSubmit={handleSubmit} className="form-inline">
+            <label className="sr-only" for="inlineFormInputName2">Release Name</label>
+            <input type="text" ref={input => _releaseName = input} className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Set the Name"/>
 
-            <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-            <div class="input-group mb-2 mr-sm-2">
-                <div class="input-group-prepend">
-                <div class="input-group-text">@</div>
+            <label className="sr-only" for="inlineFormInputGroupUsername2">Release Date</label>
+            <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
                 </div>
-                <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username"/>
+                <input type="text" ref={input => _releaseDate = input} className="form-control" id="inlineFormInputGroupUsername2" placeholder="Set the Date"/>
             </div>
 
-            <div class="form-check mb-2 mr-sm-2">
-                <input class="form-check-input" type="checkbox" id="inlineFormCheck"/>
-                <label class="form-check-label" for="inlineFormCheck">
-                Remember me
-                </label>
-            </div>
-
-            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            <button type="submit" className="btn btn-primary mb-2">Save</button>
         </form>
     );
 }
